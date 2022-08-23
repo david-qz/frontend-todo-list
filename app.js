4ee08c3204e392db77e1d53d3844cc87115a8807
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-23 12:20:06 -0700

    Implement sign in form

--- app.js
+++ app.js
@@ -8,8 +8,22 @@ createAuthForm(document.querySelector('#sign-up-form'), {
     submitHandler: handleSignUp
 });
 
+createAuthForm(document.querySelector('#sign-in-form'), {
+    submitHandler: handleSignIn
+});
+
 async function handleSignUp(email, password) {
     const response = await UserService.signUp({ email, password });
+
+    if (response.ok) {
+        location.replace('/todos');
+    } else {
+        return (await response.json()).message;
+    }
+}
+
+async function handleSignIn(email, password) {
+    const response = await UserService.signIn({ email, password });
 
     if (response.ok) {
         location.replace('/todos');
--- services/UserService.js
+++ services/UserService.js
@@ -14,6 +14,21 @@ async function signUp(userInfo) {
     return response;
 }
 
+async function signIn(userInfo) {
+    const response = await fetch(API_BASE + '/api/v1/users/sessions', {
+        method: 'Post',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+        credentials: 'include'
+    });
+
+    return response;
+}
+
 export default {
-    signUp
+    signUp,
+    signIn
 };
